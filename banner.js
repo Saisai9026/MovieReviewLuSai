1092cdeb9c3876e764cde6a45ab77500850778a4
Saisai9026 <saisai0090.ss@gmail.com>
2026-04-15 16:01:07 +0630

    Add files via upload

---
+++ banner.js
@@ -0,0 +1,20 @@
+const banners = [
+  {
+    title: "The Dark Knight",
+    desc: "Batman နဲ့ Joker ကြားက Gotham မြို့ကို ကယ်တင်တဲ့ အဆုံးစွန်တိုက်ပွဲဇာတ်လမ်း",
+    image: "https://image.tmdb.org/t/p/w1280/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
+    youtube: "https://www.youtube.com/watch?v=EXeTwQWrcwY"
+  },
+  {
+    title: "Inception",
+    desc: "အိမ်မက်အတွင်းထဲကို ဝင်ရောက်တဲ့ mind-bending thriller ဇာတ်ကား",
+    image: "https://image.tmdb.org/t/p/w1280/9O7gLzmreU0nGkIB6K3BsJbzvNv.jpg",
+    youtube: "https://www.youtube.com/watch?v=YoHD9XEInc0"
+  },
+  {
+    title: "Interstellar",
+    desc: "လူသားမျိုးနွယ်အတွက် အာကာသခရီးသွားပြီး အနာဂတ်ကို ကယ်တင်တဲ့ဇာတ်ကား",
+    image: "https://image.tmdb.org/t/p/w1280/6aNKD81RHR1DqUUa8kOZ1TBY1Lp.jpg",
+    youtube: "https://www.youtube.com/watch?v=zSWdZVtXT7E"
+  }
+];
